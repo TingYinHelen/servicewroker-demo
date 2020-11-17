494e2aed7f5a400d5cd687c52a1a6ce5d414a9c2
helen <yinhelen.hlj@qq.com>
2020-11-17 16:03:43 +0800

    修改demo

--- serviceworker.js
+++ serviceworker.js
@@ -1,16 +1,26 @@
 self.addEventListener('install', function(event) {
     console.log('install------');
     event.waitUntil(
-        caches.open('cache').then((cache) => {
-            return cache.add('./offline.html');
+        caches.open('static-v1').then((cache) => {
+            return cache.addAll(['/pig-gif.gif', '/index.html']);
         })
     );
 });
+
+self.addEventListener('activate', (event) => {
+    console.log('activate------');
+});
+
 self.addEventListener('fetch', function(event) {
-    console.log('fetch-----');
-    event.respondWith(
-        fetch(event.request).catch(() => {
-            return caches.match('./offline.html');
-        })
-    );
+    console.log('event:fetch', event);
+    console.log('event.request.url', event.request.url);
+    const url = new URL(event.request.url);
+    if (url.origin === location.origin) {
+        if (url.pathname === '/pig.jpg') {
+            event.respondWith(caches.match('/pig-gif.gif'));
+        }
+        if (url.pathname === '/') {
+            event.respondWith(caches.match('/index.html'));
+        }
+    }
 });

6c7dc5a25aaaed230d6f47674b03bd813d234ad4
helen <yinhelen.hlj@qq.com>
2020-11-17 16:55:48 +0800

    修改publicPath

--- serviceworker.js
+++ serviceworker.js
@@ -1,14 +1,17 @@
+const publicPath = '/servicewroker-demo/';
+
 self.addEventListener('install', function(event) {
     console.log('install------');
     event.waitUntil(
         caches.open('static-v1').then((cache) => {
-            return cache.addAll(['/pig-gif.gif', '/index.html']);
+            return cache.addAll([`${publicPath}pig-gif.gif`, `${publicPath}index.html`]);
         })
     );
 });
 
 self.addEventListener('activate', (event) => {
     console.log('activate------');
+    clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
@@ -16,11 +19,11 @@ self.addEventListener('fetch', function(event) {
     console.log('event.request.url', event.request.url);
     const url = new URL(event.request.url);
     if (url.origin === location.origin) {
-        if (url.pathname === '/pig.jpg') {
-            event.respondWith(caches.match('/pig-gif.gif'));
+        if (url.pathname === `${publicPath}pig.jpg`) {
+            event.respondWith(caches.match(`${publicPath}pig-gif.gif`));
         }
-        if (url.pathname === '/') {
-            event.respondWith(caches.match('/index.html'));
+        if (url.pathname === publicPath) {
+            event.respondWith(caches.match(`${publicPath}index.html`));
         }
     }
 });
